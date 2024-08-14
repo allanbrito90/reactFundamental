e830713330915010424d5acfe0ecfbe02a05efe0
Allan Brito <dsi-ab@baieuropa.pt>
2024-08-14 14:15:16 +0100

    Finishing example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 import AnotherComponent from './components/AnotherComponent';
-
 import FirstComponent from './components/FirstComponent';
+import ImagesLisboa from './components/ImagesLisboa';
+import Hooks from './components/Hooks';
+import List from './components/List';
+import RenderCond from './components/RenderCond';
+import Fragment from './components/Fragment';
+import Container from './components/Container';
 
 function App() {
   return (
@@ -9,6 +14,14 @@ function App() {
       <h2>Hello React</h2>
       <FirstComponent/>
       <AnotherComponent />
+      <ImagesLisboa />
+      <Hooks />
+      <List />
+      <RenderCond x={6} y={10}/>
+      <Fragment />
+      <Container> 
+          <h1>Este é filho do container</h1>
+      </Container>
     </div>
   );
 }
---
+++ src/components/ImagesLisboa.js
@@ -0,0 +1,12 @@
+import praca from '../assets/images.jfif';
+
+const ImagesLisboa = () => {
+    return(
+        <div>
+            <img src="/Cidade_Lisboa.jpg" />
+            <img src={praca} />
+        </div>
+    )
+}
+
+export default ImagesLisboa;
